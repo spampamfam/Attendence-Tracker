7e7c1847e45708b7d56023b3f35e9ad14fe27774
spampamfam <saiftamer994@gmail.com>
2025-12-20 17:40:06 +0200

    fixed vercel deployment error

--- client/src/components/UI/container/RectangleContainer.jsx
+++ client/src/components/UI/container/RectangleContainer.jsx
@@ -1,8 +1,7 @@
 export default function RectangleContainer({ children }) {
   return (
     <div
-      className="bg-child-container w-[calc(35%)] h-[150px] border-black shadow-md
-    shadow-black/20 rounded-lg p-6"
+      className="bg-child-container w-[calc(35%)] h-[150px] border-black shadow-md shadow-black/20 rounded-lg p-6"
       onClick={(e) => e.stopPropagation()}
     >
       {children}
