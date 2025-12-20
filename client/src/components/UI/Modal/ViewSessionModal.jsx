61e22f44cc4843bc9748eb0fa10175eb9a954cab
spampamfam <saiftamer994@gmail.com>
2025-12-20 17:33:35 +0200

    fixed vercel deployment error

--- client/src/components/UI/Modal/ViewSessionModal.jsx
+++ client/src/components/UI/Modal/ViewSessionModal.jsx
@@ -2,13 +2,15 @@ import ModalContainer from "../container/ModalContainer";
 import ModalOverlay from "../overlay/ModalOverlay";
 
 const ViewSessionModal = () => {
-  <>
-    <ModalOverlay>
-      <ModalContainer>
-        <h1>hey</h1>
-      </ModalContainer>
-    </ModalOverlay>
-  </>;
+  return (
+    <>
+      <ModalOverlay>
+        <ModalContainer>
+          <h1>hey</h1>
+        </ModalContainer>
+      </ModalOverlay>
+    </>
+  );
 };
 
 export default ViewSessionModal;
